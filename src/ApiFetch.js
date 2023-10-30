1600aba96f6103f5b3f1ccbe3a027c42bff6b7c1
Anastasia <z.anastasia11@gmail.com>
2023-10-30 02:37:10 -0700

    create form

--- src/ApiFetch.js
+++ src/ApiFetch.js
@@ -5,27 +5,43 @@ const ApiFetch = () => {
   const [url, setUrl] = useState('');
   const [data, setData] = useState(null);
 
-  const async fetchData = () => {
-    const response = await fetch(url);
-
-    const jsonData = await response.json();
-    setData(jsonData)
-  }
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Network Error");
+      }
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error(error);
+      setData(null);
+    }
+  };
 
   return (
     <div>
       <h1>Api Data Fetcher</h1>
       <form onSubmit={(e) => {
         e.preventDefault();
-        console.log("Onsubmit");
+        fetchData();
       }}>
-        <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} required>
-        </input>
+        <label>
+          <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} required>
+          </input>
+        </label>
+       <button type="submit">SUBMIT</button>
       </form>
       <h2>
         Display Data
       </h2>
-
+      
+      {data && (
+        <div>
+          <h3>Fetched Data:</h3>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
     </div>
   );
 
